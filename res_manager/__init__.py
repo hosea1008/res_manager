2061a38ef41111088a1a59dc02bc67b8f5bdb554
hosea1008 <hongshan1008@163.com>
2018-12-26 22:31:04 +0800

    update version number

--- res_manager/__init__.py
+++ res_manager/__init__.py
@@ -1,5 +1,5 @@
 from .manager import ResultManager
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __name__ = "ResultManager"
 __author__ = "Hongshan Li"
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='res_manager',
-    version='1.0.2',
+    version='1.0.3',
     description=(
         'This tool helps you to manage your experimental results in a more elegant way, saving your experiment data by pickle with some high-level interfaces to save, load and preview.'
     ),
